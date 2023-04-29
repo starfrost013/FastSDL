2dd6d0898e3230e25a385a4ab9430ee4ae97e39c
starfrost013 <mario64crashed@gmail.com>
2023-04-29 12:48:11 +0100

    nvm

--- FastSDL.h
+++ FastSDL.h
@@ -6,5 +6,9 @@
 #pragma once
 
 #include <iostream>
+#include "SDL/include/SDL.h"
+#include "SDL_image/SDL_image.h"
+#include "SDL_mixer/include/SDL_mixer.h"
+
 
 // TODO: Reference additional headers your program requires here.
